d8248fa573e1a261e97dcd79de1c74781b1edc0e
Hugo Freire <ahugo.freire@gmail.com>
2023-04-21 11:26:23 -0300

    add chat servie in golang

--- chatservice/cmd/chatservice/main.go
+++ chatservice/cmd/chatservice/main.go
@@ -1,1 +1,74 @@
-package chatservice
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/ahugofreire/chatservice/configs"
+	"github.com/ahugofreire/chatservice/internal/infra/grpc/server"
+	"github.com/ahugofreire/chatservice/internal/infra/repository"
+	"github.com/ahugofreire/chatservice/internal/infra/web"
+	"github.com/ahugofreire/chatservice/internal/infra/web/webserver"
+	"github.com/ahugofreire/chatservice/internal/usecase/chatcompletion"
+	"github.com/ahugofreire/chatservice/internal/usecase/chatcompletionstream"
+	"github.com/sashabaranov/go-openai"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	config, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&multiStatements=true",
+		config.DBUser, config.DBPassword, config.DBHost, config.DBPort, config.DBName))
+
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	repo := repository.NewChatRepositoryMySQL(conn)
+	client := openai.NewClient(config.OpenAIApiKey)
+
+	chatConfig := chatcompletion.ChatCompletionConfigInputDTO{
+		Model:                config.Model,
+		ModelMaxTokens:       config.ModelMaxTokens,
+		Temperature:          float32(config.Temperature),
+		TopP:                 float32(config.TopP),
+		N:                    config.N,
+		Stop:                 config.Stop,
+		MaxTokens:            config.MaxTokens,
+		InitialSystemMessage: config.InitialChatMessage,
+	}
+
+	chatConfigStream := chatcompletionstream.ChatCompletionConfigDTO{
+		Model:                config.Model,
+		ModelMaxTokens:       config.ModelMaxTokens,
+		Temperature:          float32(config.Temperature),
+		TopP:                 float32(config.TopP),
+		N:                    config.N,
+		Stop:                 config.Stop,
+		MaxTokens:            config.MaxTokens,
+		InitialSystemMessage: config.InitialChatMessage,
+	}
+
+	useCase := chatcompletion.NewChatCompletionUseCase(repo, client)
+
+	streamChannel := make(chan chatcompletionstream.ChatCompletionOutputDTO)
+	useCaseStream := chatcompletionstream.NewChatCompletionUseCase(repo, client, streamChannel)
+
+	fmt.Println("Server running on port " + config.GRPCServerPort)
+	grpcServer := server.NewGRPCServer(*useCaseStream, chatConfigStream, config.GRPCServerPort, config.AuthToken, streamChannel)
+	go grpcServer.Start()
+
+	webServer := webserver.NewWebServer(":" + config.WebServerPort)
+	webServerChatHandler := web.NewWebChatGPTHandler(*useCase, chatConfig, config.AuthToken)
+	webServer.AddHandler("/chat", webServerChatHandler.Handle)
+
+	fmt.Println("Server running on port " + config.WebServerPort)
+	webServer.Start()
+
+}
--- chatservice/internal/infra/grpc/server/server.go
+++ chatservice/internal/infra/grpc/server/server.go
@@ -1,1 +1,76 @@
 package server
+
+import (
+	"github.com/ahugofreire/chatservice/internal/infra/grpc/pb"
+	"github.com/ahugofreire/chatservice/internal/infra/grpc/service"
+	"github.com/ahugofreire/chatservice/internal/usecase/chatcompletionstream"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"net"
+)
+
+type GRPCServer struct {
+	ChatCompletionStreamUseCase chatcompletionstream.ChatCompletionUserCase
+	ChatConfigStream            chatcompletionstream.ChatCompletionConfigDTO
+	ChatService                 service.ChatService
+	Port                        string
+	AuthToken                   string
+	StreamChannel               chan chatcompletionstream.ChatCompletionOutputDTO
+}
+
+func NewGRPCServer(
+	chatCompletionStreamUseCase chatcompletionstream.ChatCompletionUserCase,
+	chatConfigStream chatcompletionstream.ChatCompletionConfigDTO,
+	port string,
+	authToken string,
+	StreamChannel chan chatcompletionstream.ChatCompletionOutputDTO,
+) *GRPCServer {
+	chatService := service.NewChatService(chatCompletionStreamUseCase, chatConfigStream, StreamChannel)
+	return &GRPCServer{
+		ChatCompletionStreamUseCase: chatCompletionStreamUseCase,
+		ChatConfigStream:            chatConfigStream,
+		ChatService:                 *chatService,
+		Port:                        port,
+		AuthToken:                   authToken,
+		StreamChannel:               StreamChannel,
+	}
+}
+
+// Interface Configurada para GRPC com stream de dados.
+func (g *GRPCServer) AuthInterceptor(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	ctx := ss.Context()
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return status.Error(codes.Unauthenticated, "metadata is not provided")
+	}
+
+	token := md.Get("authorization")
+	if len(token) == 0 {
+		return status.Error(codes.Unauthenticated, "authorization token is not provided")
+	}
+
+	if token[0] != g.AuthToken {
+		return status.Error(codes.Unauthenticated, "authorization token is invalid")
+	}
+
+	return handler(srv, ss)
+}
+
+func (g *GRPCServer) Start() {
+	opts := []grpc.ServerOption{
+		grpc.StreamInterceptor(g.AuthInterceptor),
+	}
+
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterChatServiceServer(grpcServer, &g.ChatService)
+
+	lis, err := net.Listen("tcp", ":"+g.Port)
+	if err != nil {
+		panic(err.Error())
+	}
+	if err := grpcServer.Serve(lis); err != nil {
+		panic(err.Error())
+	}
+}
